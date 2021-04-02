3dd788cc41e2cc53d093060be4c8439e419ccb38
Rezzo <Rezzo>
2021-04-01 21:31:26 -0400

    - Player Look Up - Inventory Value
    - As per suggestion by user Capt_Nemo, total value of a players inventory will now by displayed in the lookup tool.
    
    Signed-off-by: Rezzo <Rezzo>

--- queries/qry_ajx_plalookup.php
+++ queries/qry_ajx_plalookup.php
@@ -4,6 +4,19 @@
     $q_user = "SELECT * FROM `bluerp_players` WHERE steam_id = '" .$q. "';";
     $qret_user = mysqli_query($cold_link, $q_user);
 
+    $user_inv_val = 0;
+    $q_useritems = "SELECT `bluerp_items`.`steam_id`, `bluerp_items`.`itemid`, `bluerp_items`.`quantity`, `bluerp_itemlist`.`price` 
+    FROM bluerp_items 
+    INNER JOIN bluerp_itemlist ON `bluerp_items`.`itemid` = `bluerp_itemlist`.`itemID` 
+    WHERE `bluerp_items`.`steam_id` = '" .$q. "'
+    ORDER BY `bluerp_items`.`itemid` ASC";
+    $qret_useritems = mysqli_query($cold_link, $q_useritems);
+
+    while($useritems_row = mysqli_fetch_array($qret_useritems)) {
+        $user_inv_val += $useritems_row['quantity']*$useritems_row['price'];
+    }
+
+
     echo '<ul class="list-group">';
     while($user_row = mysqli_fetch_array($qret_user)) {
 
@@ -26,8 +39,9 @@
         $m=number_format($m,0);
 
         echo "<li class='list-group-item d-flex justify-content-between align-items-center'>Name: ". $user_row['username']."</li>";
-        echo "<li class='list-group-item d-flex justify-content-between align-items-center'>Bank: $". number_format($user_row['bank']) ."</li>";
+        echo "<li class='list-group-item d-flex justify-content-between align-items-center'>Bank: $". number_format($user_row['bank'], 2) ."</li>";
         echo "<li class='list-group-item d-flex justify-content-between align-items-center'>Income: $". number_format($user_row['income']) ."</li>";
+        echo "<li class='list-group-item d-flex justify-content-between align-items-center'>Inventory: $". number_format($user_inv_val, 2) ."</li>";
         echo "<li class='list-group-item d-flex justify-content-between align-items-center'>Playtime: " . $d . " days, " . $h . " hours, " . $m . " minutes</li>";
         echo "<li class='list-group-item d-flex justify-content-between align-items-center'>Experience: ". number_format($user_row['experience']) ."</li>";
         echo "<li class='list-group-item d-flex justify-content-between align-items-center'>Respect: ". number_format($user_row['respect']) ."</li>";
